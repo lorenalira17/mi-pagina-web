08a3a682fae9314a38d44abd162f9f29d387249f
lorenalira17 <loreflira17@gmail.com>
2025-09-18 08:37:53 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// =========================
-// JavaScript 
-// =========================
-
-// Solo un ejemplo para comprobar que funciona
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("JS cargado correctamente - Profe Joako");
-});
