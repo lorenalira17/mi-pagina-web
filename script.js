5599eefca0c60e8f50cdba80ea378f5528e0bd2b
lorenalira17 <loreflira17@gmail.com>
2025-09-18 07:59:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// =========================
+// JavaScript 
+// =========================
+
+// Solo un ejemplo para comprobar que funciona
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JS cargado correctamente - Profe Joako");
+});
